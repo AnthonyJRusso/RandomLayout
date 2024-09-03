5a58af3834d35abc932699271ae1c728a1375ff0
AnthonyJRusso <russoanthony621@gmail.com>
2024-09-03 11:01:46 -0400

    layout complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Allows speed to be set in scene
+    public float moveSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //4-directional movement for player
+        float x_value = (Input.GetAxisRaw("Horizontal") * moveSpeed);
+        float y_value = (Input.GetAxisRaw("Vertical") * moveSpeed);
+
+        transform.position += new Vector3(x_value * Time.deltaTime, y_value * Time.deltaTime, 0f);
     }
 }
